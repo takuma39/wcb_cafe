8797497bfd22b09d1145638ae4c29e3ee2b834aa
takuma39 <[takuma.sato.3939@gmail.com]>
2020-09-08 13:55:39 +0000

    Add Devise_admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+  }
+
+
   root 'homes#top'
   resources :contacts, only:[:index]
   resources :menus, only:[:index]
